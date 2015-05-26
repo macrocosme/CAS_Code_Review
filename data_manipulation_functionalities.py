f0d803c135be6c9a1961ec99c0b4b5213d12564c
macrocosme <macrocosme@users.noreply.github.com>
2015-05-26 10:49:34 +1000

    Update and rename list_path_and_files.py to data_manipulation_functionalities.py
    Added two extra functions: find_replace (as in any text editor to be used from your code) and flip_CSV (which like its name implies, flips a csv file).

--- data_manipulation_functionalities.py
+++ data_manipulation_functionalities.py
@@ -1,6 +1,14 @@
 from os import walk
 from os import listdir, walk
 from os.path import isfile, join
+from itertools import izip
+import fileinput
+import csv
+
+'''
+Simple handy functionalities to manipulate and deal with data
+from your python code. 
+'''
 
 def list_files_with_paths_recursively(mypath):
     """ Recursively list files in mypath and returns the list in the form of ['path/to/file/myfile.extension', '...'] """
@@ -14,3 +22,14 @@ def list_files_with_paths_recursively(mypath):
 def list_files_in_current_path(path):
     """ Returns files in the current folder only """
     return [ f for f in listdir(path) if isfile(join(path,f)) ]
+
+def find_replace(filename, textToSearch,textToReplace):
+    """ finds textToSearch in filename and replaces it with textToReplace """
+    i = 0
+    for line in fileinput.input(filename, inplace=True):
+        sys.stdout.write(line.replace(textToSearch, textToReplace))
+
+def flip_CSV(file):
+    """ flips a CSV file within itself (similar to a transpose) """
+    a = izip(*csv.reader(open(file, "rb")))
+    csv.writer(open(file, "wb")).writerows(a)
