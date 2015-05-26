7f77439ea28a0638bbc6b889a86d0e9612455c9a
macrocosme <macrocosme@users.noreply.github.com>
2015-05-26 10:12:09 +1000

    Update list_path_and_files.py

--- list_path_and_files.py
+++ list_path_and_files.py
@@ -1,4 +1,7 @@
 from os import walk
+from os import listdir, walk
+from os.path import isfile, join
+
 def list_path_and_files(mypath):
     """ Recursively list files in mypath and returns the list in the form of ['path/to/file/myfile.extension', '...'] """
     my_files = []
@@ -7,3 +10,7 @@ def list_path_and_files(mypath):
             for f in filenames:
                 my_files.append(dirpath + '/' + f)
     return my_files
+
+def getFiles(path):
+    """ Returns files in the current folder only """
+    return [ f for f in listdir(path) if isfile(join(path,f)) ]
