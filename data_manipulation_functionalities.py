fff411cb13384291ae6b2bac11cde58417765f9f
macrocosme <danyvohl@hotmail.com>
2016-07-07 11:10:13 +1000

    Added function to create fits cube from folder of 2D fits

--- data_manipulation_functionalities.py
+++ data_manipulation_functionalities.py
@@ -5,6 +5,10 @@ from itertools import izip
 import fileinput
 import csv
 
+from astropy.io import fits
+import pyfits
+import numpy as np
+
 '''
 Simple handy functionalities to manipulate and deal with data
 from your python code. 
@@ -33,3 +37,23 @@ def flip_CSV(file):
     """ flips a CSV file within itself (similar to a transpose) """
     a = izip(*csv.reader(open(file, "rb")))
     csv.writer(open(file, "wb")).writerows(a)
+
+def create_cube_from_files_in_current_folder(out_fname):
+    """
+    Create a cube from fits files in a folder.
+
+    :param out_fname: output filename (should be .fits)
+    :return: nothing.
+    """
+    #TODO add assert for fits extension; check that all files are in folder are fits... etc.
+    files = list_files_in_current_path('.')
+    image = fits.open(files[0])
+    cube = np.ndarray(shape=(len(files), image[0].data.shape[0], image[0].data.shape[1]), dtype=float)
+    i = 0
+    for fname in files:
+        c = fits.open(fname)
+        cube[i] = c[0].data
+        i += 1
+
+    header = pyfits.getheader(files[0])
+    pyfits.writeto(out_fname, cube, header, clobber=True)
